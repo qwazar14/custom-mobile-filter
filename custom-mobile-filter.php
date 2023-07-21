8d7ccfd7ce59319befaac8668710bb63fec83f90
qwazar14 <zenez89@gmail.com>
2023-07-21 11:57:17 +0300

    Init Commit

---
+++ custom-mobile-filter.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Custom Mobile Filter
+Description: Adds a filter button for mobile screens that triggers the BeRocket AJAX Product Filters.
+Author: Maksym "Qwazar" Mezhyrytskyi
+Version: 1.0.0
+Author URI: https://github.com/qwazar14/
+Plugin URI: https://github.com/qwazar14/custom-mobile-filter
+*/
+
